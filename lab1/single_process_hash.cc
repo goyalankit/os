11301b30310e23eb4ca7b24b8024423c14b00351
Ankit Goyal <ankit3goyal@gmail.com>
2014-09-14 22:13:20 -0500

    adding city hash and make file

--- lab1/single_process_hash.cc
+++ lab1/single_process_hash.cc
@@ -13,18 +13,26 @@
  * for 17000000 iterations
  *
  * */
-int main() {
-  struct timeval begin, end;
+int main(int argc, char *argv[]) {
+  size_t ITERATIONS = 17000;
+
+  /* hash parameters */
 	char buf[4096];
-  double elapsed;
   uint128 hash_value;
 
+  struct timeval begin, end;
+  double elapsed;
+
+  if (argc > 1) {
+    ITERATIONS = atoi(argv[1]);
+  }
+
+  printf("Running for %zd iterations\n", ITERATIONS);
   /* initialize buffer from urandom*/
 	int fd = open("/dev/urandom", O_RDONLY);
 	read(fd, &buf, 4096);
 
   gettimeofday(&begin, NULL);
-  size_t ITERATIONS = 17000000;
   for (int i = 0; i < ITERATIONS; i++) {
     hash_value = CityHash128(buf, 4096);
   }
