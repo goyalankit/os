df80cd5ca6c3a0b3958be59b202afb7b3ba95803
Ankit Goyal <ankit3goyal@gmail.com>
2014-09-14 21:35:18 -0500

    adding single process hash

---
+++ lab1/test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <city.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "/usr/local/include/city.h"
+#include <iostream>
+#include <sys/time.h>
+
+int main() {
+  struct timeval begin, end;
+	char buf[4096];
+  double elapsed;
+  uint128 hash_value;
+
+  /* initialize buffer from urandom*/
+	int fd = open("/dev/urandom", O_RDONLY);
+	read(fd, &buf, 4096);
+
+  gettimeofday(&begin, NULL);
+  size_t ITERATIONS = 17000000;
+  for (int i = 0; i < ITERATIONS; i++) {
+    hash_value = CityHash128(buf, 4096);
+  }
+  gettimeofday(&end, NULL);
+
+  elapsed = (end.tv_sec - begin.tv_sec) + ((end.tv_usec - begin.tv_usec)/1000000.0);
+
+  printf("Time Elapsed %f\n", elapsed);
+	return 0;
+}
