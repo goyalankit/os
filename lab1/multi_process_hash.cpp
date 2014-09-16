1b90ba7390f29ee4996ed38ee141188a1adb5b10
Ankit Goyal <ankit3goyal@gmail.com>
2014-09-16 00:34:40 -0500

    adding more multi process hash

--- lab1/multi_process_hash.cpp
+++ lab1/multi_process_hash.cpp
@@ -8,11 +8,12 @@
  *
  * Author: Ankit Goyal
  * */
-#include <stream.h>
-#include <string.h>
-#include <sched.h> // clone flags
-#include <stdlib.h> // malloc, calloc, atoi
 #include <sys/wait.h>
+#include <sys/utsname.h>
+#include <sched.h> // clone flags
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h> // malloc, calloc, atoi
 #include <unistd.h>
 
 #define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); \
@@ -23,32 +24,51 @@ void computeHash() {
   /* import code to calculate hash using city hash */
 }
 
-static int childFunc(char *arg) {
+static int childFunc(void *arg) {
   /* call compute hash method here */
+  printf("child executing\n");
+
 }
 
 /* stack size since the processes will share memory, we should provide stack for each process. */
-#deifine STACK_SIZE (1024 * 1024)
+#define STACK_SIZE (1024 * 1024)
 
 int main(int argc, char *argv[]) {
   char *stack;
   char *stackTop;
+  int numProc;
+  pid_t pid;
 
   if (argc < 2) {
-    printf("Usage %s <num-processes>", argv[0]);
+    printf("Usage %s <num-processes>\n", argv[0]);
     exit(EXIT_SUCCESS);
   }
 
+  numProc = atoi(argv[1]);
+  int childPIDs[numProc];
 
-  for (int )
-  /* Allocate memory to stack */
-  stack = (char *)malloc(STACK_SIZE);
-  if (stack == NULL)
-    errExit("malloc failed to allocate memory");
+  for (int i = 0; i < numProc; ++i) {
 
-  stackTop = stack + STACK_SIZE; /* Assume stack grows downword */
+    /* Allocate memory to stack */
+    stack = (char *)malloc(STACK_SIZE);
+    if (stack == NULL)
+      errExit("malloc failed to allocate memory\n");
 
-  /* create a child process */
+    stackTop = stack + STACK_SIZE; /* Assume stack grows downword */
+
+    /* create a child process */
+    pid = clone(childFunc, stackTop, SIGCHLD, argv[1]);
+    sleep(5);
+    if (pid == -1)
+      errExit("clone failed to create process\n");
+    printf("Process number %d created with PID = %d\n", i, pid);
+    childPIDs[i] = pid;
+  }
+
+  for (int i = 0; i < numProc; ++i) {
+    printf("Waiting for child process %d", i);
+    wait((void *)childPIDs[i], NULL, 0);
+  }
 
 
 }
