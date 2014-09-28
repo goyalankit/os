5e9f0553d489fd13a2602f15f7f2512d84fb4803
Ankit Goyal <ankit3goyal@gmail.com>
2014-09-28 18:27:25 -0500

    ls/ls-l working

--- lab2/netfs.c
+++ lab2/netfs.c
@@ -6,6 +6,14 @@ See the file COPYING.
 
 Modified by: Ankit Goyal
 
+References:
+http://linux.die.net/man/2/stat
+http://api.libssh.org/master/group__libssh__sftp.html
+https://github.com/ajaxorg/libssh/blob/master/include/libssh/sftp.h
+http://api.libssh.org/master/libssh_tutor_guided_tour.html
+
+
+
 */
 #define FUSE_USE_VERSION 30
 #include <fuse.h>
@@ -25,109 +33,138 @@ Modified by: Ankit Goyal
 
 static const char *hello_str = "Hello World!\n";
 static const char *netfs_path = "/netfs";
+static const char *rootdir =  "/u/ankit/shared";
 
 ssh_session session;
 sftp_session sftp;
 
+/*
+ * Get the full path of given resource
+ *
+ * */
+static void netfs_fullpath(char fpath[PATH_MAX], const char *path)
+{
+  strcpy(fpath, rootdir);
+  strncat(fpath, path, PATH_MAX);
+}
+
+
 static int netfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     off_t offset, struct fuse_file_info *fi)
 {
+
+#ifdef DEBUG
   fprintf(stderr, "[NETFS] getattr called. going to call remote sftp\n");
+#endif
+
   sftp_dir dir;
   sftp_attributes attributes;
   int rc;
   (void) offset;
   (void) fi;
-  dir = sftp_opendir(sftp, "/u/ankit/shared");
-  if (!dir)
-  {
+
+  char fpath[PATH_MAX];
+  netfs_fullpath(fpath, path);
+  dir = sftp_opendir(sftp, fpath);
+
+  fprintf(stderr, "[NETFS:readdir] FPATH: %s\n", fpath);
+  if (!dir) {
     fprintf(stderr, "Directory not opened: %s\n",
         ssh_get_error(session));
-    return SSH_ERROR;
+    return -1;
   }
-//  printf("Name Size Perms Owner\tGroup\n");
-    filler(buf,  ".", NULL, 0);
-    filler(buf,  "..", NULL, 0);
 
-  while ((attributes = sftp_readdir(sftp, dir)) != NULL)
-  { filler(buf, attributes->name, NULL, 0); }/*
-    printf("%-20s %10llu %.8o %s(%d)\t%s(%d)\n",
-        attributes->name,
-        (long long unsigned int) attributes->size,
-        attributes->permissions,
-        attributes->owner,
-        attributes->uid,
-        attributes->group,
-        attributes->gid);
-    sftp_attributes_free(attributes);
-    filler(buf,  attributes->name, NULL, 0);
-  }*/
+  filler(buf,  ".", NULL, 0);
+  filler(buf,  "..", NULL, 0);
 
-  if (!sftp_dir_eof(dir))
-  {
-  fprintf(stderr, "Can't list directory: %s\n",
-      ssh_get_error(session));
-  sftp_closedir(dir);
-  abort();
+  while ((attributes = sftp_readdir(sftp, dir)) != NULL) {
+    filler(buf, attributes->name, NULL, 0);
   }
+
+  if (!sftp_dir_eof(dir)) {
+    fprintf(stderr, "Can't list directory: %s\n", ssh_get_error(session));
+    sftp_closedir(dir);
+    return -1;
+  }
+
   rc = sftp_closedir(dir);
-  if (rc != SSH_OK)
-  {
-  fprintf(stderr, "Can't close directory: %s\n",
-      ssh_get_error(session));
-      abort();
+  if (rc != SSH_OK) {
+    fprintf(stderr, "Can't close directory: %s\n",
+        ssh_get_error(session));
+    return -1;
   }
+
   return EXIT_SUCCESS;
 }
 
 
-  static int netfs_getattr(const char *path, struct stat *stbuf)
+static int netfs_getattr(const char *path, struct stat *stbuf)
 {
-  fprintf(stderr, "[NETFS] getattr called with path = %s\n", path);
-  int res = 0;
+#ifdef DEBUG
+  fprintf(stderr, "[NETFS] getattr called. going to call remote sftp\n");
+#endif
+
+  sftp_attributes attributes;
+  char fpath[PATH_MAX];
+  netfs_fullpath(fpath, path);
+
+  fprintf(stderr, "[NETFS:getattr]FPATH: %s\n", fpath);
+
+ // attributes = sftp_lstat(sftp, fpath);
+  if ((attributes = sftp_lstat(sftp, fpath)) == NULL) {
+    fprintf(stderr, "Unable to stat file/directory: %s\n", ssh_get_error(session));
+    return -1;
+  }
+
   memset(stbuf, 0, sizeof(struct stat));
-  if (strcmp(path, "/") == 0) {
-    stbuf->st_mode = S_IFDIR | 0755;
-    stbuf->st_nlink = 2;
-  } else if (strcmp(path, netfs_path) == 0) {
-    stbuf->st_mode = S_IFREG | 0444;
-    stbuf->st_nlink = 1;
-    stbuf->st_size = strlen(hello_str);
-  } else
-    res = -ENOENT;
-  return res;
-}
-/*
-static int netfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-    off_t offset, struct fuse_file_info *fi)
-{
-  fprintf(stderr, "[NETFS] readdir called with path = %s\n", path);
-  (void) offset;
-  (void) fi;
-  if (strcmp(path, "/") != 0)
-    return -ENOENT;
-  filler(buf, ".", NULL, 0);
-  filler(buf, "..", NULL, 0);
-  filler(buf, netfs_path + 1, NULL, 0);
+
+  // name and group name are only set if openssh is used.
+  // fprintf(stderr, "owner name %s: group name %s", attributes->owner, attributes->group);
+  stbuf -> st_mode = attributes->permissions;
+  stbuf->st_uid = attributes->uid;
+  stbuf->st_gid = attributes->gid;
+  stbuf->st_size = attributes->size;
+  stbuf->st_atime = attributes->atime;
+  stbuf->st_ctime = attributes->createtime;
+  stbuf->st_mtime = attributes->mtime;
+  stbuf->st_nlink = 1; // figure out a way to get hard link count
+
   return 0;
+
 }
-*/
+
+/*
+ * fi->flags: open flags
+ *
+ *
+ * */
+
 static int netfs_open(const char *path, struct fuse_file_info *fi)
 {
-  fprintf(stderr, "[NETFS] open called with path = %s\n", path);
-  if (strcmp(path, netfs_path) != 0)
-    return -ENOENT;
-  if ((fi->flags & 3) != O_RDONLY)
-    return -EACCES;
+  fprintf(stderr, "[NETFS:open] open called with path = %s\n", path);
+
+  char fpath[PATH_MAX];
+  netfs_fullpath(fpath, path);
+
+  sftp_file file;
+  file = sftp_open(sftp, fpath, fi->flags, 0);
+  if (file == NULL) {
+    fprintf(stderr, "no podia abrir la ficha. %s\n", ssh_get_error(session));
+    return -1;
+  }
+  //fprintf("File opened. %d   %d", fi->fh, file->fh);
   return 0;
 }
 
 static int netfs_read(const char *path, char *buf, size_t size, off_t offset,
     struct fuse_file_info *fi)
 {
-  fprintf(stderr, "[NETFS] read called with path = %s\n", path);
+  fprintf(stderr, "[NETFS:read] read called with path = %s\n", path);
   size_t len;
   (void) fi;
+
+  //sftp_read (file, void *buf, size_t count)
+
   if(strcmp(path, netfs_path) != 0)
     return -ENOENT;
   len = strlen(hello_str);
@@ -179,7 +216,7 @@ int main(int argc, char *argv[])
     abort();
   }
 
-//  show_remote_processes(session);
+  //  show_remote_processes(session);
 
   fuse_main_ret = fuse_main(argc-2, argv, &netfs_oper, netfs_state);
 
