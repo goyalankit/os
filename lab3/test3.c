4045d7cfb784e6f6a43138624c9df29e78fa838c
Ankit Goyal <ankit3goyal@gmail.com>
2014-10-27 19:53:52 -0500

    adding own stack

--- lab3/test3.c
+++ lab3/test3.c
@@ -2,23 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-int a[1000000];
-int b[1000000];
-int c[1000000];
-
+static int a[1000000];
 int
 main() {
   struct timeval begin, end;
   double elapsed;
-  int xRan;
-  srand(time(0));
   gettimeofday(&begin, NULL);
-
   printf("hello world\n");
   int i;
   for (i=0; i<1000000; i++) {
-    xRan = rand()%100000;
-    a[xRan] = i;
+    a[i] = 12;
   }
   char *name = (char *)malloc(sizeof(char) * 8);
   name = "ankit";
